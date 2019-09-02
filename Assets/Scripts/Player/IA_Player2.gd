00d6484f9410bf871745cc1b687bce131bcc95f3
Anto <justantorami@gmail.com>
2019-09-03 01:36:50 +0200

    Menu Background started implementation

--- Assets/Scripts/Player/IA_Player2.gd
+++ Assets/Scripts/Player/IA_Player2.gd
@@ -3,15 +3,19 @@ extends "res://Assets/Scripts/Player/Pad.gd"
 var BallPos : Vector2
 var BallDir : Vector2
 var NewPosition : float
-var margin : int = 120
-# Easy mode -> margin = 150
+var margin : int = 50 
+# Easy -> margin = 150
+# Normal -> margin = 120
+var CanMove : bool = false
 
 func _process(delta: float) -> void:
 	BallPos = DataManager.BallPos
 	BallDir = DataManager.BallDir
 	
-	#Only move if the ball is in its half of the screen and moving to the pad
-	if BallPos.x > screen_size.x / 2 and BallDir.x > 0 :
+	#Only move if the ball is in its half of the screen and in the rigth half of the screen
+	CanMove = BallDir.x > 0 #BallPos.x > screen_size.x / 2
+	
+	if CanMove:
 		move()
 	
 func move() -> void:
